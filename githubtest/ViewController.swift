20709eb919462a7e4c03ff53c4de0881e2c85e83
azuz <zwzk5726@gmail.com>
2026-02-10 13:29:47 +0300

    Initial Commit

---
+++ githubtest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  githubtest
+//
+//  Created by abdulaziz on 10/02/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ githubtestTests/githubtestTests.swift
@@ -0,0 +1,17 @@
+//
+//  githubtestTests.swift
+//  githubtestTests
+//
+//  Created by abdulaziz on 10/02/2026.
+//
+
+import Testing
+@testable import githubtest
+
+struct githubtestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
